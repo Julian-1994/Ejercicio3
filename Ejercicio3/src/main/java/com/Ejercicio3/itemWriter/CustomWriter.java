00c696f5de46a78db923603dfd5e21200538b69d
Julian-1994 <julian.perez-perena.ext@inetum.com>
2025-04-15 08:48:41 +0200

    Add files via upload

---
+++ Ejercicio3/src/main/java/com/Ejercicio3/itemWriter/CustomWriter.java
@@ -0,0 +1,34 @@
+package com.Ejercicio3.itemWriter;
+
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import com.Ejercicio3.model.Employee;
+import javax.sql.DataSource;
+
+@Component
+public class CustomWriter implements ItemWriter<Employee> {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CustomWriter() {
+		this.jdbcTemplate = null;
+	}
+    
+    public CustomWriter(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+	@Override
+	public void write(Chunk<? extends Employee> chunk) throws Exception {
+		for (Employee item : chunk) {
+			int updated = jdbcTemplate.update("DELETE FROM employee WHERE id = ?", item.getuserId());
+			if (updated > 0) {
+				System.out.println("Deleted employee with ID: " + item.getuserId());
+			}
+		}
+		
+	}
+}
